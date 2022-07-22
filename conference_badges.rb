52cb7e280943f26ead7e2cc73d1dad54244ee9cd
Mike Shippy <mikeship04@gmail.com>
2022-07-21 17:39:17 -0700

    commit tree c919788a31c935709c9a3a785181bc6201dded55

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array_of_speakers)
+    array_of_speakers.map.with_index do |name, value|
+        "Hello, #{name}! You'll be assigned to room #{value + 1}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |i|
+        puts i
+    end
+end
